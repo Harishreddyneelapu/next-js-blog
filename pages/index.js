3fd6e276e7f6eeb76ab48bae3b81e6b18f295f55
Harishreddyneelapu <neelapuharishreddy1@gmail.com>
2025-12-05 15:59:54 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Harish Reddy, im a software developer and a cricketer</p>
+        <p>Hi, I'm Harish Reddy, im a software developer</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
